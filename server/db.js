4bff81c5fee4c8c497e1dddc70d269cafdf9bc9f
AllenHsing3 <allen.hsing@gmail.com>
2020-10-16 21:50:39 -0700

    Finished routes

--- server/db.js
+++ server/db.js
@@ -1,7 +1,12 @@
 const Pool = require('pg').Pool
 const config = require('config')
 
-const pool = ({
+const pool = new Pool ({
     user: 'allen',
-    password: config.get('psqlSecret')
+    password: config.get('psqlSecret'),
+    host: 'localhost',
+    port: 5432,
+    database: 'perntodo'
 })
+
+module.exports = pool
